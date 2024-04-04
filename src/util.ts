b8171e100c7e76c236984bf1557fe6d49c79e511
EskiMojo14 <ben.j.durrant@gmail.com>
2024-04-04 19:17:07 +0100

    Use assertion function to trim code

--- src/util.ts
+++ src/util.ts
@@ -4,3 +4,12 @@ export type Compute<T> = { [K in keyof T]: T[K] } & unknown;
 export function capitalise(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function assert(
+  condition: boolean,
+  message: string | (() => string),
+): asserts condition {
+  if (!condition) {
+    throw new Error(typeof message === "string" ? message : message());
+  }
+}
