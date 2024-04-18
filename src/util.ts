f4cb704ea66f7c54d2da6e260528679a1c6dcf00
Ben Durrant <ben.j.durrant@gmail.com>
2024-04-18 22:22:26 +0100

    Support a normal Context instance

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
 export type Compute<T> = { [K in keyof T]: T[K] } & unknown;
 
+type KeyofUnion<T> = T extends any ? keyof T : never;
+
+export type OneOf<
+  U,
+  K extends KeyofUnion<U> = KeyofUnion<U>,
+> = U extends infer T
+  ? Compute<T & { [P in Exclude<K, keyof T>]?: undefined }>
+  : never;
+
 export function capitalise(str: string) {
   if (typeof str !== "string") return "";
   return str.charAt(0).toUpperCase() + str.slice(1);
